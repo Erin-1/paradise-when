150818d51fe8c3505f65cf912894e09388ce488f
Erin Mitchell <erinmitchell232@gmail.com>
2018-05-16 17:18:25 +0800

    models set up

---
+++ db/migrate/20180516091118_create_users_answers.rb
@@ -0,0 +1,10 @@
+class CreateUsersAnswers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users_answers do |t|
+      t.references :user, foreign_key: true
+      t.references :question, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
